4c949faac73c9fbd8e0671b9bfc5dadae3d52bb6
liuyang <liuyang@uu898.com>
2025-03-10 13:00:12 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ 2025study/59带有参数的装饰器.py
@@ -0,0 +1,19 @@
+#目前加减法的函数，没有对应的符号进行判断再输出内容
+def mark(flag):
+    def out_func(need_func): # need_func 此时代表要运行的函数add、sub
+        def inner_func(num1,num2):
+            print(f"输入的装饰器实参为{flag}，正在计算中。。。")
+            print(f"两个数字的运算结果为:{need_func(num1,num2)}")
+        return inner_func
+    return out_func
+
+@mark("+")
+def add(a,b):
+    return a+b
+
+@mark("-")
+def sub(a,b):
+    return a-b
+
+
+add(3,4)
